71da3513b54d650943831c1618138acbbfb31cba
gdarquie <gaetan.darquie@gmail.com>
2019-12-16 22:56:58 +0100

    EDO-30 : create fragments in test DB with phpunit

--- tests/Integration/DefaultControllerTest.php
+++ tests/Integration/DefaultControllerTest.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Functional;
+namespace App\Tests\Integration;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 
-class FragmentTest extends ApiTestCase
+class DefaultControllerTest extends ApiTestCase
 {
-    public function testGetFragments()
+    public function testGetDefault()
     {
         $client = self::createClient();
-        $client->request('GET', 'api/fragments');
+        $client->request('GET', '/');
         $this->assertResponseStatusCodeSame(200);
     }
 }
