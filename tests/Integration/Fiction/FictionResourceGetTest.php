f222475a353d9347e853b5a2b0f9dfdbb1457ce1
gdarquie <gaetan.darquie@gmail.com>
2020-02-25 14:50:10 +0100

    FI-89: add test for homepage

--- tests/Integration/Fiction/FictionResourceGetTest.php
+++ tests/Integration/Fiction/FictionResourceGetTest.php
@@ -2,31 +2,24 @@
 
 declare(strict_types=1);
 
-
 namespace App\Tests\Integration\Fiction;
 
+use App\Tests\Integration\AbstractIntegrationTest;
 
-class FictionResourceGetTest
+class FictionResourceGetTest extends AbstractIntegrationTest
 {
     public function testGetFiction() {
         $fictionUuid = '1b7df281-ae2a-40bf-ad6a-ac60409a9ce6';
 
-        $response = $this->client->request();
-    }
-
-    public function testGetNarrativeWithFragments()
-    {
-        $uuid = '6284e5ac-09cf-4334-9503-dedf31bafdd0';
-
-        $response = $this->client->request('GET', 'api/narratives/'.$uuid);
-
+        $response = $this->client->request('GET', 'api/fictions/'.$fictionUuid);
         $this->assertResponseIsSuccessful();
         $arrayResponse = $response->toArray();
 
-        $this->assertEquals(2, count($arrayResponse['fragments']));
-        $this->assertEquals($arrayResponse['fragments'][1]['title'], 'Fragment title');
-        $this->assertEquals($arrayResponse['fragments'][0]['title'], 'Fragment title 2');
-        $this->assertEquals($arrayResponse['uuid'], '6284e5ac-09cf-4334-9503-dedf31bafdd0');
-        $this->assertEquals($arrayResponse['content'], $arrayResponse['fragments'][0]['content'], 'Not the correct content');
+        $this->assertEquals($arrayResponse['title'], 'Fiction Title', 'Wrong fiction title');
+        $this->assertEquals($arrayResponse['content'], '', 'Wrong fiction content');
+        $this->assertEquals(2, count($arrayResponse['narratives']), 'Wrong narratives number.');
+        $this->assertEquals(1, count($arrayResponse['origins']), 'Wrong origins number.');
+        $this->assertEquals(1, count($arrayResponse['followings']), 'Wrong followings number');
+        $this->assertEquals(0, count($arrayResponse['characters']), 'Wrong characters number');
     }
 }
