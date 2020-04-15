8944538f0c4ac4163c9d5b0ea621566f397a3724
gdarquie <gaetan.darquie@gmail.com>
2020-04-16 01:12:00 +0200

    FI-169: reorder route can handle change of parent or position

--- src/Component/DTO/Strategy/Reorder/ReorderDTOPost.php
+++ src/Component/DTO/Strategy/Reorder/ReorderDTOPost.php
@@ -9,12 +9,14 @@ namespace App\Component\DTO\Strategy\Reorder;
 use App\Component\DTO\Strategy\DTOStrategyConfig;
 use App\Component\DTO\Strategy\DTOStrategyInterface;
 use App\Component\DTO\Tree\PositionConvertor;
+use App\Entity\Narrative;
 use App\Entity\Position;
 
 class ReorderDTOPost implements DTOStrategyInterface
 {
     /**
      * @param DTOStrategyConfig $config
+     * @throws \Exception
      */
     public function proceed(DTOStrategyConfig $config)
     {
@@ -26,28 +28,25 @@ class ReorderDTOPost implements DTOStrategyInterface
         $createdAt = $position->getCreatedAt();
 
         // we remove from the tree only the selected narrative (not its children), the narrative will be deleted but all of the tree info will be updated correctly
-//        $em->getRepository(Position::class)->removeFromTree($position);
-//        $em->clear();
-        // todo: check if everything is ok = left and right change correctly when using simple doctrine remove and not removeFromTree (be carefull, there is a warning in comments in doctrine extension for using removeFromTree)
-        $em->remove($position);
-        $em->flush();
+        $em->getRepository(Position::class)->removeFromTree($position); // removeFromTree is unsafe according to Gedmo doc
+        $em->clear();
 
+        // create the new position
+        $newPosition = new Position();
+        $newPosition->setCreatedAt($createdAt);
         // change the parent of the narrative, if it's null, it means there is no parent
         $parentPosition = PositionConvertor::getParentPositionFromNarrativeUuid($config->getDto()->getParentUuid(), $em);
-        $position->setParent($parentPosition);
-        // we restore the first creation date of the position before we deleted it
-        $position->setCreatedAt($createdAt);
+        $newPosition->setParent($parentPosition);
+        $narrativeUuid = $config->getDto()->getNarrativeUuid();
+        $positionNarrative = $em->getRepository(Narrative::class)->findOneByUuid($narrativeUuid);
+        $newPosition->setNarrative($positionNarrative);
 
-        $narrative->setPosition($position);
-        $em->persist($position);
-        $em->persist($narrative);
-
-        // save entity
+        $em->persist($newPosition);
         $em->flush();
 
-        // we set the position of the narrative in the node
-        //todo: set the narrative position, how ???
-
+        // place the narrative
+        $childrenCount = count($parentPosition->getChildren());
+        $em->getRepository(Position::class)->moveUp($newPosition, $childrenCount-$config->getDto()->getPosition());
     }
 
 }
